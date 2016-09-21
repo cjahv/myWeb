9775399aab9eab67e8e2c5190376ed7e779643ab
jahv <cjahv@qq.com>
2016-09-21 16:55:46 +0800

    新增数据库连接

---
+++ conf/db.js
@@ -0,0 +1,13 @@
+/**
+ * Created by jahv on 2016/9/21.
+ */
+// MySQL数据库联接配置
+module.exports = {
+    mysql: {
+        host: '127.0.0.1',
+        user: 'root',
+        password: '',
+        database:'test',
+        port: 3306
+    }
+};
